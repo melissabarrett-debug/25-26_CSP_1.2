09216db50cfaf2d9ffb132ba3ef8c8a9b8f4a80c
Melissa B <melissa.barrett@vtsd.com>
2025-12-05 13:07:15 -0500

    turtle game

--- 1.2.1/Game update 2.py
+++ 1.2.1/Game update 2.py
@@ -7,15 +7,37 @@ import random as rand
 #-----game configuration----
 spot_color = "purple"
 score=0
-score_writer=trtl.Turtle()
+font_setup= ("Arial", 20, "normal")
+
 #-----initialize turtle-----
+score_writer=trtl.Turtle()
+score_writer.penup()
+box_turtle=trtl.Turtle()
+
+#shape turtle
 meowl = trtl.Turtle()
 meowl.shape("circle")
 meowl.color(spot_color)
 meowl.shapesize(3)
-meowl.penup()
+box_turtle.penup()
 
 #-----game functions--------
+#draw the box for the score
+def scoreBox():
+    box_turtle.goto(375, 325)
+    box_turtle.pendown()
+    #draw box
+    for sides in range(2):
+        box_turtle.forward(100)
+        box_turtle.left(90)
+        box_turtle.forward(50)
+        box_turtle.left(90)
+    #place score box
+    score_writer.penup()
+    score_writer.goto(300, 350)
+    #hide turtles
+    score_writer.hideturtle()
+    box_turtle.hideturtle()
 # Get a score boost, move the turtle randomly
 def spot_clicked(x, y):
     change_position()
@@ -30,11 +52,12 @@ def change_position():
 def update_score():
     global score
     score+=1
-    print(score)
+    score_writer.clear()
+    score_writer.write(score, font=font_setup)
 #-----events----------------
 meowl.onclick(spot_clicked)
 
 
-
+scoreBox()
 wn = trtl.Screen()
 wn.mainloop()
