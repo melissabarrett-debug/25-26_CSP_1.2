28ac369c0ace428865bbbd04fb2dc1db5d22f5ee
Melissa B <melissa.barrett@vtsd.com>
2025-12-11 13:27:18 -0500

    leaderboard

--- 1.2.2/turtle_game.py
+++ 1.2.2/turtle_game.py
@@ -1,7 +1,8 @@
+#1.1.2 File
 #-----import statements-----
 import turtle as trtl
 import random as rand
-
+import leaderboard as lb
 
 
 #-----game configuration----
@@ -15,6 +16,10 @@ colors=["red", "green", "blue", "yellow", "purple", "orange"]
 
 
 #-----initialize turtle-----
+leaderboard_file_name = "a122_leaderboard.txt"
+player_name = input("what is your name?")
+
+#score turtle
 score_writer=trtl.Turtle()
 score_writer.penup()
 box_turtle=trtl.Turtle()
@@ -92,10 +97,31 @@ def countdown():
   if timer <= 0:
     counter.write("Time's Up", font=font_setup)
     timer_up = True
+    manage_leaderboard()
   else:
     counter.write("Timer: " + str(timer), font=font_setup)
     timer -= 1
     counter.getscreen().ontimer(countdown, counter_interval)
+# CODE TO ADD
+# Add this function to your game code
+
+# manages the leaderboard for top 5 scorers
+def manage_leaderboard():
+
+  global score
+  global meowl
+
+  # get the names and scores from the leaderboard file
+  leader_names_list = lb.get_names(leaderboard_file_name)
+  leader_scores_list = lb.get_scores(leaderboard_file_name)
+
+  # show the leaderboard with or without the current player
+  if (len(leader_scores_list) < 5 or score >= leader_scores_list[4]):
+    lb.update_leaderboard(leaderboard_file_name, leader_names_list, leader_scores_list, player_name, score)
+    lb.draw_leaderboard(True, leader_names_list, leader_scores_list, meowl, score)
+
+  else:
+    lb.draw_leaderboard(False, leader_names_list, leader_scores_list, meowl, score)
 meowl.onclick(spot_clicked)
 scoreBox()
 
