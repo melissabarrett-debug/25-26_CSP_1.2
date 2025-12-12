eb656e99f17339da6a9855d1715003e7120f2d12
Melissa B <melissa.barrett@vtsd.com>
2025-12-12 12:27:32 -0500

    leaderboard

--- 1.2.2/leaderboard.py
+++ 1.2.2/leaderboard.py
@@ -19,6 +19,10 @@ def get_names(file_name):
         index = 0
 
         # TODO 1: use a while loop to read the leader name from the line (format is "leader_name,leader_score")
+        while (line[index] != ","):
+            leader_name = leader_name + line[index]
+            index = index + 1
+
 
         # TODO 2: add the player name to the names list
 
