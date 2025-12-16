95b9a9e1cd403a12c38baac405976b881f29feba
Melissa B <melissa.barrett@vtsd.com>
2025-12-16 13:51:14 -0500

    leaderboard steps 35-46

--- 1.2.2/leaderboard.py
+++ 1.2.2/leaderboard.py
@@ -66,22 +66,27 @@ def get_scores(file_name):
 def update_leaderboard(file_name, leader_names, leader_scores, player_name, player_score):
     index = 0
     # TODO 8: loop through all the scores in the existing leaderboard list
-    '''
-    for   :
+
+    for index in range(len(leader_scores)):
       # TODO 9: check if this is the position to insert new score at
-      if ():
+      if (player_score>=leader_scores[index]):
         break
       else:
         index = index + 1
-    '''
+
 
     # TODO 10: insert new player and score
+    leader_scores.insert(index, player_score)
+    leader_names.insert(index, player_name)
 
     # TODO 11: keep both lists at 5 elements only (top 5 players)
+    if len(leader_scores) > 5:
+        player_name.pop()
+        player_score.pop()
 
     # TODO 12: store the latest leaderboard back in the file
 
-    '''
+
     leaderboard_file = open(file_name, "w")  # this mode opens the file and erases its contents for a fresh start
   
     # TODO 13 loop through all the leaderboard elements and write them to the the file
@@ -89,7 +94,6 @@ def update_leaderboard(file_name, leader_names, leader_scores, player_name, play
       leaderboard_file.write(leader_names[index] + "," + str(leader_scores[index]) + "\n")
   
     leaderboard_file.close()
-     '''
 
 
 # draw leaderboard and display a message to player
