3b2981cc7d2f7391fc8abe6e1fdba6135f4e357b
Melissa B <melissa.barrett@vtsd.com>
2025-12-17 13:09:14 -0500

    Unit 1.2 update

---
+++ parameter_practice/example1.py
@@ -0,0 +1,21 @@
+
+'''# methods to add numbers
+def add_numbers(one, two):
+    sum = one + two
+    return sum
+    '''
+def operation(num1, num2, num3):
+    result= (num1+ num2) * num3
+    return result
+
+def void_operation(num1, num2, num3):
+    result= (num1+ num2) * num3
+    print(result)
+
+answer=void_operation(3, 4, 5)
+print(answer)
+
+
+
+
+
