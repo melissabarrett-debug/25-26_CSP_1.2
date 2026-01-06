2514561051624a285eab86d654878783a698e5fa
Melissa B <melissa.barrett@vtsd.com>
2026-01-06 11:46:11 -0500

    Unit 1.2 update

--- 1.2.3/main.py
+++ 1.2.3/main.py
@@ -1,29 +1,46 @@
-#   a123_apple_1.py
 import turtle as trtl
 
-#-----setup-----
+letter_list=["a"]
+
 apple_image = "apple.gif" # Store the file name of your shape
+ground_height = -200
+apple_letter_x_offset = -25
+apple_letter_y_offset = -50
 
 wn = trtl.Screen()
 wn.setup(width=1.0, height=1.0)
 wn.addshape(apple_image) # Make the screen aware of the new file
 
+
+wn.bgpic("background.gif")
 apple = trtl.Turtle()
+apple.penup()
+wn.tracer(False)
 
-
-#-----functions-----
 # given a turtle, set that turtle to be shaped by the image file
 def draw_apple(active_apple):
   active_apple.shape(apple_image)
-
+  draw_letter("A", active_apple)
   wn.update()
 
 def drop_apple():
-    apple.goto(apple.xcor(), -200)
+    wn.tracer(True)
+    apple.goto(apple.xcor(), ground_height)
+    wn.tracer(False)
+    apple.clear()
+    apple.hideturtle()
 
-#-----function calls-----
+def draw_letter(letter, active_apple):
+  active_apple.color("white")
+  remember_position = active_apple.position()
+  active_apple.setpos(active_apple.xcor() + apple_letter_x_offset,active_apple.ycor() + apple_letter_y_offset)
+  active_apple.write(letter, font=("Arial", 74, "bold"))
+  active_apple.setpos(remember_position)
+
 draw_apple(apple)
 wn.onkeypress(drop_apple, "a")
 
+
+
 wn.listen()
-wn.mainloop()
+trtl.mainloop()
